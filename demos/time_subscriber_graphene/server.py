b14dcaba590968028a7537a45d6012558d1460d3
Rob Blackbourn <rob.blackbourn@gmail.com>
2020-10-07 16:57:24 +0100

    Updated graphene subscription

--- demos/time_subscriber_graphene/server.py
+++ demos/time_subscriber_graphene/server.py
@@ -1,10 +1,11 @@
 """Time Subscriber Server"""
 
+import uvicorn
+
 from bareasgi import Application
 from bareasgi_graphql_next.graphene import add_graphene
-from demos.time_subscriber_graphene.time_schema import SCHEMA
 
-import uvicorn
+from demos.time_subscriber_graphene.time_schema import SCHEMA
 
 app = Application()
 add_graphene(app, SCHEMA)
--- demos/time_subscriber_graphene/time_schema.py
+++ demos/time_subscriber_graphene/time_schema.py
@@ -6,18 +6,22 @@ from datetime import datetime
 from graphene import ObjectType, String, Schema, Field
 
 # All schema require a query.
+
+
 class Query(ObjectType):
     hello = String()
 
     def resolve_hello(root, info):
         return 'Hello, world!'
 
+
 class Subscription(ObjectType):
     time = Field(String)
 
     async def subscribe_time(root, info):
         while True:
-            yield { 'time': datetime.now().isoformat()}
+            yield datetime.now().isoformat()
             await asyncio.sleep(1)
+
 
 SCHEMA = Schema(query=Query, subscription=Subscription)
